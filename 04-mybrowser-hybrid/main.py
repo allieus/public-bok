483c8463a8f394af6df2fcc64b2f830d66494c60
Chinseok Lee <allieuslee@gmail.com>
2015-04-24 02:26:10 +0900

    [20150424] Hybrid 테스트용, index.html 페이지 추가

--- 04-mybrowser-hybrid/main.py
+++ 04-mybrowser-hybrid/main.py
@@ -1,9 +1,13 @@
+import os
 import sys
 from PyQt5 import uic
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidgetItem
 
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+
 URLS = [
+    ('Hybrid 테스트', 'file://' + os.path.join(ROOT_PATH, 'index.html')),
     ('네이버', 'http://m.naver.com'),
     ('다음', 'http://m.daum.net'),
     ('네이트', 'http://m.nate.com'),
@@ -31,11 +35,14 @@ class Window(QMainWindow):
         name, url = URLS[current_row]
         self.ui.webView.load(QUrl(url))
 
+
 def load_stylesheet():
     return open('main.css', 'rb').read().decode('utf8')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     app.setStyleSheet(load_stylesheet())
     window = Window()
     sys.exit(app.exec_())
+
